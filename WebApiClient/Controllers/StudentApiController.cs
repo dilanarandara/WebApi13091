8de330c77db86c320a7ff577980e5728fe17021a
Dilan Arandara <dilan.madu@gmail.com>
2013-09-22 14:17:12 +0530

    Changed connection string. Add StudentApi Controller. Add database context.

---
+++ WebApiClient/Controllers/StudentApiController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApiClient.Models;
+
+namespace WebApiClient.Controllers
+{
+    public class StudentApiController : ApiController
+    {
+        private ApiTestContext db = new ApiTestContext();
+
+        // GET api/StudentApi
+        public IEnumerable<Student> GetStudents()
+        {
+            return db.Students.AsEnumerable();
+        }
+
+        // GET api/StudentApi/5
+        public Student GetStudent(Guid id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return student;
+        }
+
+        // PUT api/StudentApi/5
+        public HttpResponseMessage PutStudent(Guid id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != student.StudentId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/StudentApi
+        public HttpResponseMessage PostStudent(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, student);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = student.StudentId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/StudentApi/5
+        public HttpResponseMessage DeleteStudent(Guid id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Students.Remove(student);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
