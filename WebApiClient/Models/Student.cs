339cc459b70bb271927f88f8ff1d5585ac3539f3
Dilan Arandara <dilan.madu@gmail.com>
2013-09-22 14:03:46 +0530

    Add class Student.

---
+++ WebApiClient/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiClient.Models
+{
+  public class Student
+  {
+    public Guid StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+  }
+}
